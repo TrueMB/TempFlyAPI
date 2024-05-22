c6d11c4b45ffe5f8c89b78152365d145742b74fe
TrueMB <>
2024-05-22 11:10:26 +0200

    Fix: Add Main class to get the initialization of the API

---
+++ src/main/java/me/truemb/tempfly/main/TempFly.java
@@ -0,0 +1,12 @@
+package me.truemb.tempfly.main;
+
+import me.truemb.tempfly.api.FlyManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TempFly extends JavaPlugin {
+	
+	public static FlyManager getFlyManager() {
+		return null;
+	}
+
+}
